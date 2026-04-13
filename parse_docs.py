d0cc70e63444f6395aa7313378d75c8ce3f863e3
wy <yanwong@126.com>
2026-04-13 22:03:21 +0800

    download parse result test

--- parse_docs.py
+++ parse_docs.py
@@ -2,9 +2,9 @@ import requests
 import os
 import argparse
 
-input_dir = 'data/finance_pdfs'
+input_dir = 'data/test_pdfs'
 output_dir = 'data'
-batch_id_filename = 'data/batch_id.txt'
+batch_id_file = 'data/batch_id.txt'
 batch_size = 10
 
 token = os.getenv('MINERU_TOKEN')
@@ -15,64 +15,56 @@ header = {
     "Authorization": f"Bearer {token}"
 }
 
-upload_files = [os.path.join(input_dir, f) for f in os.listdir(input_dir)]
-
-upload_data = {
-    "model_version": "vlm",
-    "language": "ch",
-    "files": [
-        {
-            "name": os.path.basename(f),
-            "data_id": os.path.splitext(os.path.basename(f))[0]
-        }
-    ]
-}
-
-for i in range(0, len(upload_files), batch_size):
-    batch_files = upload_files[i:i+batch_size]
-    batch_data = {
-        'model_version': 'vlm',
-        'language': 'ch',
-        'files': [
-            {
-                'name': os.path.basename(f),
-                'data_id': os.path.splitext(os.path.basename(f))[0]
-            } for f in batch_files
-        ]
-    }
-    batch_ids = []
-    try:
-        response = requests.post(upload_url, headers=header, json=batch_data)
-        if response.status_code == 200:
-            result = response.json()
-            print(f'Response success: {result}')
-            if result['code'] == 0:
-                for url, path in zip(result['data']['file_urls'], batch_files):
-                    with open(path, 'rb') as f:
-                        res_upload = requests.put(url, data=f)
-                        if res_upload.status_code == 200:
-                            print(f"{urls[i]} upload success")
-                            batch_ids.append(result['data']['batch_id'])
-                        else:
-                            print(f"{urls[i]} upload failed")
-            else:
-                print(f'Apply upload url failed: {result["msg"]}')
-        else:
-            print(f'Response failed: {response}')
-    except Exception as err:
-        print(err)
-
-
-# batch_id = 'd979fcb6-2a43-4a48-b647-65ee84098394'
-# url = f"https://mineru.net/api/v4/extract-results/batch/{batch_id}"
-# header = {
-#     "Content-Type": "application/json",
-#     "Authorization": f"Bearer {token}"
-# }
+# upload_files = [os.path.join(input_dir, f) for f in os.listdir(input_dir)]
 # 
-# res = requests.get(url, headers=header)
-# print(res.status_code)
-# print(res.json())
-# print(res.json()["data"])
+# batch_ids = []
+# for i in range(0, len(upload_files), batch_size):
+#     batch_files = upload_files[i:i+batch_size]
+#     batch_data = {
+#         'model_version': 'vlm',
+#         'language': 'ch',
+#         'files': [
+#             {
+#                 'name': os.path.basename(f),
+#                 'data_id': os.path.splitext(os.path.basename(f))[0]
+#             } for f in batch_files
+#         ]
+#     }
+#     try:
+#         response = requests.post(upload_url, headers=header, json=batch_data)
+#         if response.status_code == 200:
+#             result = response.json()
+#             print(f'Response success: {result}')
+#             if result['code'] == 0:
+#                 batch_ids.append(result['data']['batch_id'])
+#                 for url, path in zip(result['data']['file_urls'], batch_files):
+#                     with open(path, 'rb') as f:
+#                         res_upload = requests.put(url, data=f)
+#                         if res_upload.status_code == 200:
+#                             print(f"{url} upload success")
+#                         else:
+#                             print(f"{url} upload failed")
+#             else:
+#                 print(f'Apply upload url failed: {result["msg"]}')
+#         else:
+#             print(f'Response failed: {response}')
+#     except Exception as err:
+#         print(err)
+# 
+# if batch_ids:
+#     with open(batch_id_file, 'w') as f:
+#         for bid in batch_ids:
+#             f.write(f'{bid}\n')
+# else:
+#     print('No batch uploaded')
+
+
+
+with open(batch_id_file, 'r') as f:
+    for batch_id in f:
+        url = f"https://mineru.net/api/v4/extract-results/batch/{batch_id}"
+        res = requests.get(url, headers=header)
+        print(res.status_code)
+        print(res.json())
 
 
